7b5e3e4b9d8e56b602231529020669b0c0574d52
szeremetakasia <szeremeta.kasia@gmail.com>
2020-11-15 16:23:36 +0100

    Prime numbers exercise

---
+++ Prime.py
@@ -0,0 +1,11 @@
+n = 100
+
+print("First", n, "prime numbers are:")
+
+for num in range(n):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
